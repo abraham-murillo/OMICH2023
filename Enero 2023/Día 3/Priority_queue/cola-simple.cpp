8235f72c5839402725b1961c8dd30abfac6e2377
Luis Fernando Rojas González <luisfernando.lzc@hotmail.com>
2023-01-11 13:19:03 -0600

    Agregué priority_queue

---
+++ Enero 2023/Día 3/Priority_queue/cola-simple.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+int main() {
+    priority_queue<int> cola;
+    int n;
+    cin >> n;
+    
+    int a;
+    while(n--)
+    {
+        cin >> a;
+        cola.push(a);
+    }
+    
+    while(!cola.empty())
+    {
+        cout << cola.top() << "\n";
+        cola.pop();
+    }
+}
